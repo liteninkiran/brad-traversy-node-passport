c9255fc63a869c0534da990c5b6d2bd2024bdc18
Kiran Anand <liteninkiran@hotmail.com>
2023-07-27 00:46:46 +0100

    15 - Connect to DB

--- app.js
+++ app.js
@@ -7,6 +7,17 @@ const app = express();
 
 const port = process.env.PORT || 5000;
 
+const db = require('./config/keys').mongoURI;
+
+// Connect to MongoDB
+mongoose
+    .connect(db, {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+    })
+    .then(() => console.log('MongoDB Connected'))
+    .catch(err => console.log(err));
+
 // EJS
 app.use(expressLayouts);
 app.set('view engine', 'ejs');
--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://' + process.env.MONGO_DB_USERNAME + ':'+ encodeURIComponent(process.env.MONGO_DB_PASSWORD) + '@' + process.env.MONGO_DB_CLUSTER + '.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://' + process.env.MONGO_DB_USERNAME + ':'+ encodeURIComponent(process.env.MONGO_DB_PASSWORD) + '@' + process.env.MONGO_DB_CLUSTER + '.dn5mbqt.mongodb.net/?retryWrites=true&w=majority';
 
 module.exports = {
     mongoURI: dbPassword,
