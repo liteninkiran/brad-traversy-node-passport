646cc5349939c67b5779e7ef5475e4990c1098ac
Kiran Anand <liteninkiran@hotmail.com>
2023-07-27 00:41:57 +0100

    14 - Add mongo URI

--- config/keys.js
+++ config/keys.js
@@ -1,4 +1,4 @@
-dbPassword = 'mongodb+srv://YOUR_USERNAME_HERE:'+ encodeURIComponent('YOUR_PASSWORD_HERE') + '@CLUSTER_NAME_HERE.mongodb.net/test?retryWrites=true';
+dbPassword = 'mongodb+srv://' + process.env.MONGO_DB_USERNAME + ':'+ encodeURIComponent(process.env.MONGO_DB_PASSWORD) + '@' + process.env.MONGO_DB_CLUSTER + '.mongodb.net/test?retryWrites=true';
 
 module.exports = {
     mongoURI: dbPassword,
