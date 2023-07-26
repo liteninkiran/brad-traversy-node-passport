7d60afe8c65d450ef5bdb27823bee6df7d736f1f
Kiran Anand <liteninkiran@hotmail.com>
2023-07-27 00:52:28 +0100

    18 - POST request

--- routes/users.js
+++ routes/users.js
@@ -7,4 +7,10 @@ router.get('/login', (req, res) => res.render('login'));
 // Register Page
 router.get('/register', (req, res) => res.render('register'));
 
+// Register
+router.post('/register', (req, res) => {
+    console.log(req.body);
+    res.send('Hello');
+});
+
 module.exports = router;
