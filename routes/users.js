a250deaee6143e014c40b49ed6423460457e9c87
Kiran Anand <liteninkiran@hotmail.com>
2023-07-27 00:55:11 +0100

    20 - Validation

--- routes/users.js
+++ routes/users.js
@@ -10,6 +10,25 @@ router.get('/register', (req, res) => res.render('register'));
 // Register
 router.post('/register', (req, res) => {
     const { name, email, password, password2 } = req.body;
+    let errors = [];
+
+    if (!name || !email || !password || !password2) {
+        errors.push({ msg: 'Please enter all fields' });
+    }
+
+    if (password != password2) {
+        errors.push({ msg: 'Passwords do not match' });
+    }
+
+    if (password.length < 6) {
+        errors.push({ msg: 'Password must be at least 6 characters' });
+    }
+
+    if (errors.length > 0) {
+
+    } else {
+        res.send('pass');
+    }
 });
 
 module.exports = router;
