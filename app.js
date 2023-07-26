ca41be178e2bbfae751447a9719f74a085fb1820
Kiran Anand <liteninkiran@hotmail.com>
2023-07-26 21:39:27 +0100

    06 - Layouts

--- app.js
+++ app.js
@@ -1,15 +1,17 @@
 const express = require('express');
+const expressLayouts = require('express-ejs-layouts');
 const dotenv = require('dotenv').config();
 
 const app = express();
 
 const port = process.env.PORT || 5000;
 
+// EJS
+app.use(expressLayouts);
+app.set('view engine', 'ejs');
+
 // Routes
 app.use('/', require('./routes/index.js'));
 app.use('/users', require('./routes/users.js'));
 
-
 app.listen(port, console.log(`Listening on ${port}`));
-
-
