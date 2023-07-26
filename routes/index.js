dec9aa0bd990259f7e9c1505be7bfa947fc87e7c
Kiran Anand <liteninkiran@hotmail.com>
2023-07-26 21:44:45 +0100

    08 - Flash messages

--- routes/index.js
+++ routes/index.js
@@ -2,6 +2,6 @@ const express = require('express');
 const router = express.Router();
 
 // Welcome Page
-router.get('/', (req, res) => res.send('Welcome'));
+router.get('/', (req, res) => res.render('welcome'));
 
 module.exports = router;
