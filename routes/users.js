d29c167cf44934eb6d8d99e6365eb1efc515537a
Kiran Anand <liteninkiran@hotmail.com>
2023-07-27 00:57:06 +0100

    22 - Pass in data

--- routes/users.js
+++ routes/users.js
@@ -26,7 +26,11 @@ router.post('/register', (req, res) => {
 
     if (errors.length > 0) {
         res.render('register', {
-
+            errors,
+            name,
+            email,
+            password,
+            password2,
         });
     } else {
         res.send('pass');
