58299682d05cfcd496f9e85ce259ee1797568e17
Kiran Anand <liteninkiran@hotmail.com>
2023-07-27 12:22:11 +0100

    25 - User model

--- routes/users.js
+++ routes/users.js
@@ -1,5 +1,7 @@
 const express = require('express');
 const router = express.Router();
+const bcrypt = require('bcryptjs');
+const User = require('../models/User');
 
 // Login Page
 router.get('/login', (req, res) => res.render('login'));
@@ -12,14 +14,17 @@ router.post('/register', (req, res) => {
     const { name, email, password, password2 } = req.body;
     let errors = [];
 
+    // Check all fields have a value
     if (!name || !email || !password || !password2) {
         errors.push({ msg: 'Please enter all fields' });
     }
 
+    // Check passwords match
     if (password != password2) {
         errors.push({ msg: 'Passwords do not match' });
     }
 
+    // Check password length
     if (password.length < 6) {
         errors.push({ msg: 'Password must be at least 6 characters' });
     }
@@ -33,8 +38,29 @@ router.post('/register', (req, res) => {
             password2,
         });
     } else {
-        res.send('pass');
+        User.findOne({ email: email }).then(user => {
+            if (user) {
+                errors.push({ msg: 'Email already exists' });
+                res.render('register', {
+                    errors,
+                    name,
+                    email,
+                    password,
+                    password2,
+                });
+            } else {
+                const newUser = new User({
+                    name,
+                    email,
+                    password,
+                });
+
+                console.log(newUser);
+
+            }
+        });
     }
+
 });
 
 module.exports = router;
