ff3aa1bc054a6c2e8eb7ce674d627615e2345923
Kiran Anand <liteninkiran@hotmail.com>
2023-07-27 12:30:36 +0100

    28 - Flash messages

--- app.js
+++ app.js
@@ -2,6 +2,8 @@ const express = require('express');
 const expressLayouts = require('express-ejs-layouts');
 const mongoose = require('mongoose');
 const dotenv = require('dotenv').config();
+const flash = require('connect-flash');
+const session = require('express-session');
 
 const app = express();
 
@@ -25,6 +27,18 @@ app.set('view engine', 'ejs');
 
 // Express body parser
 app.use(express.urlencoded({ extended: false }));
+
+// Express session
+app.use(
+    session({
+        secret: 'secret',
+        resave: true,
+        saveUninitialized: true,
+    })
+);
+
+// Connect flash
+app.use(flash());
 
 // Routes
 app.use('/', require('./routes/index.js'));
